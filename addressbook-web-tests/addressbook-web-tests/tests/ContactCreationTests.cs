dc2c0c8715fafa25e28ece70317a6ee6f23b9df3
Goncharenko Filipp <phil.can@yandex.ru>
2022-04-18 00:20:29 +0300

    Задание №15: реализовать загрузку тестовых данных из файла

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Text;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -20,7 +24,40 @@ namespace WebAddressbookTests
             return contact;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromCsvFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1])
+                {
+                    Address = parts[2],
+                    Email = parts[3],
+                    Email2 = parts[4],
+                    Email3 = parts[5],
+                    HomePhone = parts[6],
+                    MobilePhone = parts[7],
+                    WorkPhone = parts[8]
+                });
+            }
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>)).Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                 File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromXmlFile")]
         public void ContactCreationTest(ContactData contact)
         {
 
