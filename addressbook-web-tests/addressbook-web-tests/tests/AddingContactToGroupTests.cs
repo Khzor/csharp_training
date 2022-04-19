121a3eeaf95ef0a9fcde7f7032e2fa33f0bee6ad
Goncharenko Filipp <phil.can@yandex.ru>
2022-04-19 16:50:08 +0300

    Добавлены проверки на существующие группы/контакты/контакты в группах

--- addressbook-web-tests/addressbook-web-tests/tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/AddingContactToGroupTests.cs
@@ -12,9 +12,25 @@ namespace WebAddressbookTests
         [Test]
         public void TestAddingContactToGroup()
         {
+            if (GroupData.GetAll().Count() == 0)
+            {
+                app.Groups.Create(new GroupData("newGroup"));
+            }
+            if (ContactData.GetAll().Count == 0)
+            {
+                app.Contacts.Create(new ContactData("qwe", "ewq"));
+            }
+
             GroupData group = GroupData.GetAll()[0];
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = ContactData.GetAll().Except(group.GetContacts()).First();
+
+            ContactData contact = ContactData.GetAll().Except(group.GetContacts()).FirstOrDefault();
+
+            if (contact == null)
+            {
+                app.Contacts.Create(new ContactData("zxc", "zxv"));
+                contact = ContactData.GetAll().Except(group.GetContacts()).First();
+            }
 
             app.Contacts.AddContactToGroup(contact, group);
 
